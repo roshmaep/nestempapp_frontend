40d2621af42dfb2ae34f2876c67843c58ccd4769
roshmaep <roshmaep1999@gmail.com>
2022-12-15 12:58:00 +0530

    security all done

---
+++ src/app/securitylogin/securitylogin.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from '../api.service';
+
+@Component({
+  selector: 'app-securitylogin',
+  templateUrl: './securitylogin.component.html',
+  styleUrls: ['./securitylogin.component.css']
+})
+export class SecurityloginComponent {
+  email=""
+  password=""
+  constructor(private api:ApiService,private route:Router){}
+ 
+  readValues=()=>
+  {
+    let data:any={"email":this.email,"password":this.password}
+    console.log(data)
+    this.api.securityLogin(data).subscribe(
+      (response:any)=>{
+        this.email=""
+        this.password=""
+        if (response.length==0) {
+          
+          alert("Invalid credential")
+        } else {
+          let userId=response.userid
+          console.log(userId)
+          localStorage.setItem("userInfo",userId)
+          //alert("Valid credential")
+          this.route.navigate(['/viewsecurityprofile'])
+          
+        }
+      }
+    )
+
+    
+
+      
+  }
+ 
+
+}
---
+++ src/app/securitynavbar/securitynavbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-securitynavbar',
+  templateUrl: './securitynavbar.component.html',
+  styleUrls: ['./securitynavbar.component.css']
+})
+export class SecuritynavbarComponent {
+
+}
